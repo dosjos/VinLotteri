82a5a4d10a7848d1a56d9a9978d6997039d8c274
Jan Ole Skotterud <j.skotterud@gmail.com>
2012-08-26 21:59:52 +0200

    Refactored, and fixed the GraphView.
    Also added the possibility to add new names to the list during runtime

--- VinLotteri/GraphWindow.cs
+++ VinLotteri/GraphWindow.cs
@@ -23,7 +23,9 @@ namespace VinLotteri
             chart1.Series["Series1"].ChartType = SeriesChartType.Bar;
             chart1.Series["Series1"].Color = Color.Blue;
             chart1.ChartAreas["ChartArea1"].AxisX.Interval = 1;
-            
+
+            ConcatinateTicketHolders(draws, names);
+
             for(int i = 0; i < draws.Length; i++){
                 if (names[i].Equals(Winner))
                 {
@@ -31,10 +33,29 @@ namespace VinLotteri
                 }
                 else
                 {
-                    chart1.Series["Series1"].Points.Add(draws[i]).AxisLabel = names[i];
+                    if (draws[i] > 0)
+                    {
+                        chart1.Series["Series1"].Points.Add(draws[i]).AxisLabel = names[i];
+                    }
                 }
             }
           }
+
+        private void ConcatinateTicketHolders(int[]draws, string[]names)
+        {
+            for (int last = names.Length-1; last > 0; last--)
+            {
+                string name = names[last];
+                for(int i = 0; i < last; i++){
+                    if(names[i].Equals(name)){
+                        draws[i] += draws[last];
+                        draws[last] = 0;
+                        names[last] = "";
+                        break;
+                    }
+                }
+            }
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
